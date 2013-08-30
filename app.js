203d559674b4a785358eb16345b4dab48209cf5f
Steve Horsfield <stevehorsfield@users.noreply.github.com>
2013-08-30 07:36:55 -0400

    removing jade

--- app.js
+++ app.js
@@ -12,8 +12,6 @@ var app = express();
 
 // all environments
 app.set('port', process.env.PORT || 3000);
-app.set('views', __dirname + '/views');
-app.set('view engine', 'jade');
 
 
 app.use(express.favicon());
